8cd624dcf4f9914b5d4d9993196abf9d8f0f79d7
Invouking a time <30478987+Invouker@users.noreply.github.com>
2025-11-23 18:12:03 +0100

    Changed main app project to core module

---
+++ NexusConnect/src/main/java/eu/invouk/nexusconnect/Nexusconnect.java
@@ -0,0 +1,46 @@
+package eu.invouk.nexusconnect;
+
+import eu.invouk.nexusconnect.connector.WebSocketManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class Nexusconnect extends JavaPlugin {
+
+    private WebSocketManager webSocketManager;
+    private static Nexusconnect instance;
+
+
+    private static final Logger log = LoggerFactory.getLogger(Nexusconnect.class);
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        //this.saveDefaultConfig(); // Ak používate konfiguračný súbor
+
+        // Načítanie URI z konfigurácie (odporúčané)
+        //String wsUri = this.getConfig().getString("websocket-uri", "ws://localhost:8080/ws/connect");
+        String wsUri = "ws://localhost:8080/ws/connect";
+        this.webSocketManager = new WebSocketManager(this, wsUri);
+        this.webSocketManager.connect();
+
+        log.info("NexusConnect: Plugin a WebSocket Manager inicializované.");
+
+    }
+
+    @Override
+    public void onDisable() {
+        if (this.webSocketManager != null) {
+            this.webSocketManager.disconnect();
+        }
+        log.info("NexusConnect: Plugin bol úspešne vypnutý.");
+    }
+
+    public static Nexusconnect getInstance() {
+        return instance;
+    }
+
+    public WebSocketManager getWebSocketManager() {
+        return webSocketManager;
+    }
+}
